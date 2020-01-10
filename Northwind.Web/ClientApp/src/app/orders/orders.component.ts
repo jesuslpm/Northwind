e2f6cbe4f8ef353a2ac6a1f5c2470bd4c63f60ac
MacMini <macmini@MacMinis-Mac-mini-2.local>
2020-01-10 16:38:15 +0530

    order listing with search form

--- Northwind.Web/ClientApp/src/app/orders/orders.component.ts
+++ Northwind.Web/ClientApp/src/app/orders/orders.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit, OnDestroy,ViewChild  } from '@angular/core';
-import { CatalogClient, OrdersClient, Order
-        } from '../clients';
+import { Component, OnInit, OnDestroy,AfterViewInit,ViewChild, TemplateRef  } from '@angular/core';
+import { CatalogClient, Product, 
+  Category, Supplier, 
+  OrdersClient, Order,CustomersClient, Customer
+} from '../clients';
+
 import { Subject } from 'rxjs';
 import { DataTableDirective } from 'angular-datatables';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { BsModalService, BsModalRef, ModalDirective } from 'ngx-bootstrap/modal';
 import { ToastrService } from 'ngx-toastr';
 import * as moment from 'moment';
 
@@ -12,14 +16,20 @@ import * as moment from 'moment';
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.css']
 })
-export class OrdersComponent implements OnInit, OnDestroy {
+export class OrdersComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  
+  products: Product[] = [];
   orders: Order[] = [];
+  categories: Category[] = [];
+  suppliers: Supplier[] = [];
+  customers: Customer[] = [];
   dtOptions: any = {};
   dtTrigger: any = new Subject();
   @ViewChild(DataTableDirective,{static: false}) datatableElement: DataTableDirective;
 
+  searchForm: FormGroup;
+  advanceSearchForm: FormGroup;
+  modalRef: BsModalRef;
   currentProduct:any = {};
   minOrderDateTo: any = null;
   isDisabledOrderDateTo: Boolean = true;
@@ -37,8 +47,10 @@ export class OrdersComponent implements OnInit, OnDestroy {
   constructor(
     private client: CatalogClient,
     private ordersClient: OrdersClient,
+    private customerClient: CustomersClient,
     private fb: FormBuilder,
-    private toastr: ToastrService,
+    private modalService: BsModalService,
+    private toastr: ToastrService
   ) { }
 
   ngOnInit() {
@@ -60,13 +72,20 @@ export class OrdersComponent implements OnInit, OnDestroy {
               }
             }
           ],
-          'columnDefs': [ 
-            { 'type': 'date', 'targets': 4 },
-            { 'type': 'date', 'targets': 5 },
-            { 'type': 'date', 'targets': 6 } 
-          ],
       };
-      this.getOrders(true);
+      
+      
+      this.getCustomers();
+
+      this.searchForm = this.fb.group({
+        orderId:[null],
+        customerIds: [''],
+        orderDateFrom: [null]
+      });
+  }
+
+  ngAfterViewInit(): void {
+    this.dtTrigger.next();
   }
 
   ngOnDestroy(): void {
@@ -78,7 +97,6 @@ export class OrdersComponent implements OnInit, OnDestroy {
     this.ordersClient.getAllOrders()
           .subscribe((orders) => {
             if(orders != null) {
-              console.log('orders',orders);
               this.orders = orders;
               this.dtTrigger.next();
             } else {
@@ -89,13 +107,86 @@ export class OrdersComponent implements OnInit, OnDestroy {
     });
   }
 
+  getCustomers() {
+    this.customerClient.getAllCustomers()
+          .subscribe(customers => {
+            if(customers != null) {
+              this.customers = customers;
+            } else {
+              this.toastr.error('Something went wrong!', 'Error!');      
+            }
+    },(err) => {
+      this.toastr.error('Something went wrong!', 'Error!');
+    });
+  }
 
   rerender(): void {
     this.datatableElement.dtInstance.then((dtInstance: DataTables.Api) => {
       // Destroy the table first
       dtInstance.destroy();
       // Call the dtTrigger to rerender again
-     // this.dtTrigger.next();
+     this.dtTrigger.next();
     });
+  }
+
+  advanceSearchSubmit() {
+    if(this.advanceSearchForm.valid) {
+      this.datatableElement.dtInstance.then((dtInstance: DataTables.Api) => {
+        dtInstance.draw();
+        this.modalRef.hide();
+      });
+    } else {
+      return false;
+    }
+  }
+
+  searchFormSubmit() {
+    if(this.searchForm.valid) {
+      let searchData:any = {};
+      if(this.searchForm.value.customerIds != "") {
+        searchData.customerIds = [this.searchForm.value.customerIds];
+      } else {
+        searchData.customerIds = [];
+      }
+      
+      if((this.searchForm.value.orderId == null) || (this.searchForm.value.orderId == '')){
+        searchData.orderId = null;
+      } else {
+        searchData.orderId = +this.searchForm.value.orderId;
+      }
+      
+      if(this.searchForm.value.orderDateFrom != null) {
+        searchData.orderDateFrom = moment(this.searchForm.value.orderDateFrom).format("YYYY-MM-DD");
+        searchData.orderDateTo = searchData.orderDateFrom;
+      } else {
+        searchData.orderDateFrom =  null;
+        searchData.orderDateTo =  null;
+      }
+      
+      searchData.employeeIds = [];
+      searchData.shipperIds = [];
+      searchData.productIds= [];
+      searchData.orderAmountFrom =  null;
+      searchData.orderAmountTo = null;
+      searchData.requiredDateFrom = null;
+      searchData.requiredDateTo = null;
+      searchData.shippedDateFrom = null;
+      searchData.shippedDateTo  = null;
+
+      this.ordersClient.search(searchData)
+          .subscribe(orders => {
+            if(orders != null) {
+              this.orders = orders;
+              //this.dtTrigger.next();
+              this.rerender();
+            } else {
+              this.toastr.error('Something went wrong!', 'Error!');      
+            }
+      },(err) => {
+        this.toastr.error('Something went wrong!', 'Error!');
+      });
+    } else {
+      return false;
+    }
   }
 }

7cfce9ad387e33e45d52953f74bc7e87d4c0393d
Jesús López Méndez <jesus.lopez@i-nercya.com>
2020-01-10 13:13:49 +0100

    Remove AddControllers (we have AdddControllersWithViews)

--- Northwind.Web/Startup.cs
+++ Northwind.Web/Startup.cs
@@ -23,7 +23,6 @@ namespace Northwind.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDataService();
-            services.AddControllers().AddNewtonsoftJson();
             services.AddControllersWithViews().AddNewtonsoftJson();
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
